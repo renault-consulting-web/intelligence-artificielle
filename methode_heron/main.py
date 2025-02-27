274af492bb5c5885959b816a2192a329b0de6b8f
Dolyveen RENAULT <renault@karudev.fr>
2025-02-27 14:51:11 +0100

    methode heron

--- methode_heron/main.py
+++ methode_heron/main.py
@@ -1,1 +1,9 @@
-print('test')
+def methode_heron(S, xn):
+    def algo(xn):
+        return 1/2 * (xn + S/xn)
+
+    xn1 = algo(xn)
+    return xn1 if xn1 == xn else methode_heron(S, xn1)
+
+x = methode_heron(50, 7)
+print(x)
